03c6858bc0715c11bb48132e17dd4bc5d07fcd60
Chenshu <chenshu@sg.local>
2016-08-15 17:35:08 +0800

    build

--- dist/libs/taskRunner.js
+++ dist/libs/taskRunner.js
@@ -5,26 +5,26 @@ Object.defineProperty(exports, "__esModule", {
 });
 
 exports.default = function (templData, callback) {
-  _Gulp2.default.task('inlineCss', ['mustache'], function () {
+  _gulp2.default.task('inlineCss', ['mustache'], function () {
 
-    return _Gulp2.default.src(OUTPUT_PATH + '*.html').pipe((0, _gulpInlineCss2.default)({
+    return _gulp2.default.src(OUTPUT_PATH + '*.html').pipe((0, _gulpInlineCss2.default)({
       applyLinkTags: true,
       applyTableAttributes: true,
       removeLinkTags: true,
       removeHtmlSelectors: true
-    })).pipe(_Gulp2.default.dest(OUTPUT_PATH));
+    })).pipe(_gulp2.default.dest(OUTPUT_PATH));
   });
 
-  _Gulp2.default.task('mustache', function () {
-    return _Gulp2.default.src(__dirname + '/../templates/*.mustache').pipe((0, _gulpMustache2.default)(templData, { extension: '.html' })).pipe(_Gulp2.default.dest(OUTPUT_PATH));
+  _gulp2.default.task('mustache', function () {
+    return _gulp2.default.src(__dirname + '/../templates/*.mustache').pipe((0, _gulpMustache2.default)(templData, { extension: '.html' })).pipe(_gulp2.default.dest(OUTPUT_PATH));
   });
 
-  _Gulp2.default.start('inlineCss', callback);
+  _gulp2.default.start('inlineCss', callback);
 };
 
-var _Gulp = require('Gulp');
+var _gulp = require('gulp');
 
-var _Gulp2 = _interopRequireDefault(_Gulp);
+var _gulp2 = _interopRequireDefault(_gulp);
 
 var _gulpInlineCss = require('gulp-inline-css');
 
